a03d3b5a6122b6e87151bb755e661b76409d779d
Mario <mario.quinonescastro@cesunbc.edu.mx>
2026-02-21 09:35:26 -0800

    Introducción a HTTP y Métodos HTTP

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -28,6 +28,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('runner.urls')),
+    path('api/auth/', include('users.urls')),
     path('api/auth/login/', TokenObtainPairView.as_view(), name='auth_login'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='auth_refresh'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
